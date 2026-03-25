034e0eef5d7b4570db9db92ffc39149a95444271
pejovicvuk <pejovic.vuki@gmail.com>
2026-03-25 21:01:30 +0100

    extra schemas deleted

--- PythonScraper/db/totalsQuery.sql
+++
@@ -1,4 +0,0 @@
- SELECT
-      (SELECT COUNT(*) FROM matches) AS matches,
-      (SELECT COUNT(*) FROM current_odds) AS odds,
-      (SELECT COUNT(*) FROM arbitrage_opportunities WHERE is_active = true) AS active_arbs;
